d7e42375259766de17ec1ccafa46ba32bf4ee2af
znotdead <zhirafchik@gmail.com>
2016-06-27 16:21:25 +0800

    heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hbapi.wsgi --log-file -
--- hbapi/settings/heroku.py
+++ hbapi/settings/heroku.py
@@ -4,3 +4,7 @@ from .base import *
 
 
 DATABASES['default'] =  dj_database_url.config()
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
