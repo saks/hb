cba8e3aa1eed671ac161d3f4ffc57769369296a2
znotdead <zhirafchik@gmail.com>
2016-06-30 17:36:21 +0800

    heroku settings hosts, admin

--- hbapi/settings/heroku.py
+++ hbapi/settings/heroku.py
@@ -5,8 +5,12 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+ADMINS = [('znotdead', 'zhirafchik@gmail.com')]
+
 DATABASES['default'] =  dj_database_url.config()
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = [".herokuapp.com",]
