2e731952b1f61ccb42a83aa721e99423b4ef388b
znotdead <zhirafchik@gmail.com>
2016-07-06 18:25:28 +0800

    better display of tags checkboxes

--- records/static/js/admin/change_records.js
+++ records/static/js/admin/change_records.js
@@ -3,7 +3,19 @@ $(function() {
 
   $('#id_transaction_type').val('EXP');
   $('#id_user').val(userId);
-  $('#id_amount_0').val('').focus();
+  if ($('#id_amount_0').val() == 0) {
+    $('#id_amount_0').val('');
+  };
+  $('#id_amount_0').focus();
 
-  $('#id_user').parents('.form-group').hide()
+  $('#id_user').parents('.form-group').hide();
+
+  // tags
+  updateTags = function(){
+    $('div#id_tags div').attr('class', 'btn btn-default');
+    $('#id_tags div input:checked').parent().parent().removeClass('btn-default');
+    $('#id_tags div input:checked').parent().parent().addClass('btn-danger');
+  };
+  updateTags();
+  $('#id_tags div').click(updateTags);
 });
