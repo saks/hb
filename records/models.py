64f1b83297147da8da16eb6a4915e6f1cacbee53
znotdead <zhirafchik@gmail.com>
2016-07-22 19:32:32 +0800

    fix tests and improve

--- records/models.py
+++ records/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import pre_save, pre_delete
 from django.core.validators import MinValueValidator
 from django.utils.translation import ugettext_lazy as _
 
@@ -57,6 +58,13 @@ class Record(models.Model):
     transaction_type = models.CharField(choices=TRANSACTION_TYPE, max_length=3)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def redis_tags_key(self):
+        '''
+            Return Redis key where stored sorted set of tags frequency usage.
+        '''
+        return settings.REDIS_KEY_USER_TAGS % (self.user_id,)
+
     def get_tags_list(self):
         return [k for k,v in self.tags.items() if v]
 
@@ -67,20 +75,36 @@ class Record(models.Model):
     def __str__(self):
         return '%s %s' % (', '.join([k for k, v in self.tags.items() if v]), self.amount)
 
+    def remove_tags_weights(self):
+        r = settings.REDIS_CONN
+        for tag in self.get_tags_list():
+            r.zincrby(self.redis_tags_key, tag, -1)
+            if not r.zscore(self.redis_tags_key, tag):
+                r.zrem(self.redis_tags_key, tag)
 
-from django.db.models.signals import pre_save
+    def add_tags_weights(self):
+        for tag in self.get_tags_list():
+            settings.REDIS_CONN.zincrby(self.redis_tags_key, tag, 1)
+
+
 def update_tags_weight(sender, **kwargs):
     instance = kwargs['instance']
     _tags_updated = False
+
+    # remove weights if update
     if instance.pk:
         orig = Record.objects.get(pk=instance.pk)
         if orig.tags.mask != instance.tags.mask:
             _tags_updated = True
-            # remove weights if update
-            for tag in orig.get_tags_list():
-                settings.REDIS_CONN.zincrby('tags_%s' % (instance.user_id,), tag, -1)
+            orig.remove_tags_weights()
+
     # add tags weight on create or update tags
     if not instance.pk or _tags_updated:
-        for tag in instance.get_tags_list():
-            settings.REDIS_CONN.zincrby('tags_%s' % (instance.user_id,), tag, 1)
+        instance.add_tags_weights()
+
+def delete_tags_weight(sender, **kwargs):
+    instance = kwargs['instance']
+    instance.remove_tags_weights()
+
 pre_save.connect(update_tags_weight, sender=Record)
+pre_delete.connect(delete_tags_weight, sender=Record)
