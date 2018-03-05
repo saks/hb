3a2fe2a58f9d9279985a3c6e50184a593831d10e
znotdead <zhirafchik@gmail.com>
2018-03-04 18:09:28 -0800

    #74 added tags, created_at, amount

--- records/serializers.py
+++ records/serializers.py
@@ -1,3 +1,5 @@
+import time
+
 from rest_framework import serializers
 
 from .models import Record
@@ -9,7 +11,27 @@ class RecordSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True,
         view_name='user-detail'
     )
+    tags = serializers.SerializerMethodField()
+    amount = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Record
         fields = ('user', 'tags', 'amount', 'transaction_type', 'created_at')
+
+    def get_tags(self, obj):
+        tags = [i[0] for i in obj.tags.items() if i[1]]
+        tags = {t: obj.tags.get_label(t) for t in tags}
+        return tags
+
+    def get_amount(self, obj):
+        return {
+            'amount': obj.amount.amount,
+            'currency': {
+                'code': obj.amount.currency.code,
+                'name': obj.amount.currency.name
+            }
+        }
+
+    def get_created_at(self, obj):
+        return time.mktime(obj.created_at.timetuple())
