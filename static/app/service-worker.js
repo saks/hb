97b868615e515e569d324177332e8b88fc9d9885
Aliaksandr Rahalevich <saks@users.noreply.github.com>
2018-03-04 16:24:34 -0800

    Add pwa template (#73)
    add pwa template, auth

---
+++ static/app/service-worker.js
@@ -0,0 +1,42 @@
+var cacheName = 'home-budget-0.0.1';
+var filesToCache = [
+    '/static/app/index.html',
+    '/static/app/scripts/app.js',
+    '/static/app/styles/inline.css',
+    '/static/app/images/ic_add_white_24px.svg',
+    '/static/app/images/ic_refresh_white_24px.svg',
+];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
