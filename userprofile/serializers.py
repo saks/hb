4d760dd22fa7f2b6bfc0fc9994e0510f1ae67acd
znotdead <zhirafchik@gmail.com>
2018-03-04 18:44:47 -0800

    #82 cleanup

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -6,26 +6,11 @@ from records.models import TAGS
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
-    records = serializers.HyperlinkedRelatedField(
-        many=True,
-        view_name='record-detail',
-        source='record_set',
-        read_only=True
-    )
-
-    budgets = serializers.HyperlinkedRelatedField(
-        many=True,
-        view_name='budget-detail',
-        source='budget_set',
-        read_only=True
-    )
-
     tags = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'is_staff',
-                  'is_superuser', 'records', 'budgets', 'tags')
+        fields = ('username', 'email', 'tags')
 
     def get_tags(self, obj):
         return dict(TAGS)
