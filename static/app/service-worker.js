3353f454ea9e5e71ed1342b37c0605a7138ea291
Aliaksandr Rahalevich <saksmlz@gmail.com>
2018-03-04 19:39:22 -0800

    cache external resources

--- static/app/service-worker.js
+++ static/app/service-worker.js
@@ -7,6 +7,14 @@ var filesToCache = [
     '/static/app/images/ic_refresh_white_24px.svg',
 ];
 
+const externalsCacheName = 'home-budget-externals-0.0.1';
+const externalUrls = [
+    'https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js',
+    'https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js',
+    'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+    'https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css',
+];
+
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
@@ -18,12 +26,37 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+    console.log('[Service Worker] Fetch', e.request.url);
+
+    if (externalUrls.includes(e.request.url)) {
+        e.respondWith(
+            caches.open(externalsCacheName).then(function(cache) {
+                return fetch(e.request).then(function(response){
+                    cache.put(e.request.url, response.clone());
+                    return response;
+                });
+            })
+        );
+    } else {
+        /*
+         * The app is asking for app shell files. In this scenario the app uses the
+         * "Cache, falling back to the network" offline strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+         */
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request);
+            })
+        );
+    }
+
+
+    // console.log('[ServiceWorker] Fetch', e.request.url);
+    // e.respondWith(
+    //     caches.match(e.request).then(function(response) {
+    //         return response || fetch(e.request);
+    //     })
+    // );
 });
 
 self.addEventListener('activate', function(e) {
