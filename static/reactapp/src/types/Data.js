4bd629edbf048fcd406866fabe4408a64d0e5600
Aliaksandr Rahalevich <saksmlz@gmail.com>
2018-08-04 01:02:43 -0700

    WIP

--- static/reactapp/src/types/Data.js
+++ static/reactapp/src/types/Data.js
@@ -1,4 +1,5 @@
 export type AuthToken = { email: string, exp: number, user_id: number, username: string };
+
 export type BudgetAttrs = {
     user: string,
     amount: string,
@@ -7,4 +8,10 @@ export type BudgetAttrs = {
     left_average_per_day: number,
     average_per_day: number,
     spent: number,
+};
+
+export type AuthErrors = {
+    username?: string,
+    password?: string,
+    non_field_errors?: string,
 };
