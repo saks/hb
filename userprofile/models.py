a5483a8b393c6354708f36183aea9a20e4650a99
znotdead <zhirafchik@gmail.com>
2018-08-18 17:01:50 -0700

    #103 order tags

--- userprofile/models.py
+++ userprofile/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     '''
-        User model.
-        Uses old table 'auth_user'.
+    User model.
+    Uses old table 'auth_user'.
     '''
     tags = ArrayField(models.CharField(max_length=30), null=True, blank=True)
 
@@ -17,14 +17,28 @@ class User(AbstractUser):
     @property
     def redis_tags_key(self):
         '''
-            Return Redis key where stored sorted set of tags frequency usage.
+        Return Redis key where stored sorted set of tags frequency usage.
         '''
         return settings.REDIS_KEY_USER_TAGS % (self.id,)
 
-    def get_user_tags_order(self):
+    def get_tags_order_by_frequency(self):
         '''
-            Returns sorted by frequency of usage tags list for current user.
+        Returns sorted by frequency of usage tags list for current user.
         '''
         tags = settings.REDIS_CONN.zrange(self.redis_tags_key, 0, -1)
         tags = [str(t, 'utf-8') for t in tags]
         return reversed(tags)
+
+    def get_ordered_tags(self):
+        '''
+        Order tags according to tags frequency in redis.
+        '''
+        ordered = self.get_tags_order_by_frequency()
+        result = []
+        for item in ordered:
+            if item in self.tags:
+                result.append(item)
+        for tag in self.tags:
+            if tag not in result:
+                result.append(tag)
+        return result
