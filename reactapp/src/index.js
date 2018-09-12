e1143628083f02c8527d4f459b117ec961fe0ea7
Aliaksandr Rahalevich <saksmlz@gmail.com>
2018-09-12 00:59:56 -0700

    WIP

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -6,7 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -17,4 +17,7 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
